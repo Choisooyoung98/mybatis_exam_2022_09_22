bba4d27c3767b27d6f9cd6dee87bdff53eeb5b1a
Choisooyoung98 <ekfms15815@gmail.com>
2022-09-22 13:12:04 +0900

    mybatis exam 세팅

---
+++ src/main/java/com/ll/exam/mybatis_exam_2022_09_22/MybatisExam20220922Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mybatis_exam_2022_09_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisExam20220922Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisExam20220922Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/mybatis_exam_2022_09_22/MybatisExam20220922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.mybatis_exam_2022_09_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisExam20220922ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
